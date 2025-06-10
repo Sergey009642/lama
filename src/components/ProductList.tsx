b6327f34ce56d1525336171d625e9fbef40811c5
Sergey009642 <sergaghasaryan@gmail.com>
2025-06-10 19:05:19 +0400

    Next.js Footer Component Design

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -28,7 +28,7 @@ const ProductList = () => {
                 <div className="text-sm text-gray-500">My description</div>
                 <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">Add to Cart</button>
             </Link>
-             <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+            <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
                 <div className="relative w-full h-80">
                     <Image
                         src="https://images.pexels.com/photos/17503375/pexels-photo-17503375.jpeg?auto=compress&cs=tinysrgb&w=600"
@@ -52,7 +52,7 @@ const ProductList = () => {
                 <div className="text-sm text-gray-500">My description</div>
                 <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">Add to Cart</button>
             </Link>
-             <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+            <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
                 <div className="relative w-full h-80">
                     <Image
                         src="https://images.pexels.com/photos/17503375/pexels-photo-17503375.jpeg?auto=compress&cs=tinysrgb&w=600"
@@ -76,7 +76,7 @@ const ProductList = () => {
                 <div className="text-sm text-gray-500">My description</div>
                 <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">Add to Cart</button>
             </Link>
-             <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+            <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
                 <div className="relative w-full h-80">
                     <Image
                         src="https://images.pexels.com/photos/17503375/pexels-photo-17503375.jpeg?auto=compress&cs=tinysrgb&w=600"
@@ -99,7 +99,7 @@ const ProductList = () => {
                 </div>
                 <div className="text-sm text-gray-500">My description</div>
                 <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white">Add to Cart</button>
-            </Link>
+            </Link>git
         </div>
     );
 };
