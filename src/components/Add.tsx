1a38c33d765e87b313bf854fc4bef9ca9fddf2c3
Sergey009642 <sergaghasaryan@gmail.com>
2025-06-14 03:32:35 +0400

    Next.js Wix Headless E-Commerce App Tutorial

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -22,9 +22,9 @@ const Add = () => {
             <div className=" flex justify-between">
                 <div className=" flex items-center gap-4">
                     <div className="bg-gray-100 py-2 px-4 rounded-3xl flex items-center justify-between w-32 ">
-                        <button className=" cursor-pointer text-xl" onClick={() => hendleQuantity("d")}>+</button>
+                        <button className=" cursor-pointer text-xl" onClick={() => hendleQuantity("d")}>-</button>
                         {quantity}
-                        <button className=" cursor-pointer text-xl" onClick={() => hendleQuantity("i")}>-</button>
+                        <button className=" cursor-pointer text-xl" onClick={() => hendleQuantity("i")}>+</button>
                     </div>
                     <div className="text-xs">
                         Only <span className=" text-orange-500">4 items</span> left!<br /> {"Dont"}{""}
